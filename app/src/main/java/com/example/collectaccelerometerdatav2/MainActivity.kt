2aa0105d2c40892836ad0f283a310c9c3e434b67
Sơn Tùng Kiều <kieusontung8@gmail.com>
2024-05-01 15:37:36 +0700

    Send sensor data to server successfully

--- app/src/main/java/com/example/collectaccelerometerdatav2/MainActivity.kt
+++ app/src/main/java/com/example/collectaccelerometerdatav2/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.collectaccelerometerdatav2
 //https://www.youtube.com/watch?v=6_wK_Ud8--0
+//import androidx.compose.ui.unit.TextUnit
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,38 +19,25 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-//import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 import com.example.collectaccelerometerdatav2.ui.theme.CollectAccelerometerDatav2Theme
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.w3c.dom.Text
-import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.net.Socket
-import kotlinx.coroutines.launch
-import androidx.compose.runtime.rememberCoroutineScope
-
-
-
 
 
 public class MainActivity : ComponentActivity(){
@@ -78,8 +66,9 @@ public class MainActivity : ComponentActivity(){
         }
 
         fun sendDataToServer(data: FloatArray){
-//        val str = data.toString()
-            val str= "["+currentData.value[0].toString()+","+currentData.value[1].toString()+","+currentData.value[1].toString()+"]"
+            val currentTimeMillis = SystemClock.elapsedRealtime()
+
+            val str= "["+data[0].toString()+","+data[1].toString()+","+data[1].toString()+"]" + "time: "+currentTimeMillis.toString()
             Log.e("send string",str)
             coroutineScope.launch {
                 withContext(Dispatchers.Default) {// Launch coroutine for network operations
@@ -139,36 +128,36 @@ public class MainActivity : ComponentActivity(){
                 }
             ){ if(!isRecording.value)Text(text = "Start")else Text(text = "Stop")}
 
-            Button(onClick = {
-                isConnecting.value = !isConnecting.value
-                if(isConnecting.value) {
-                    dataArray.clear()
-                    coroutineScope.launch {
-                        withContext(Dispatchers.Default) {// Launch coroutine for connection
-                            try {
-                                socket.setSoTimeout(500)
-                                socket.connect(InetSocketAddress("192.168.1.110", 8000))
-                                val message = "connected".encodeToByteArray()
-                                socket.outputStream.write(message)
-                                socket.outputStream.flush()
+            Button(
+                onClick = {
+                    isConnecting.value = !isConnecting.value
+                    if (isConnecting.value) {
+                        dataArray.clear()
+                        coroutineScope.launch {
+                            withContext(Dispatchers.Default) {// Launch coroutine for connection
+                                try {
+                                    socket.setSoTimeout(500)
+                                    socket.connect(InetSocketAddress("192.168.1.110", 8000))
+                                    val message = "connected".encodeToByteArray()
+                                    socket.outputStream.write(message)
+                                    socket.outputStream.flush()
 
-                                // Update isConnecting state based on success (optional)
-                            } catch (e: Exception) {
-                                Log.e(
-                                    "Connection Error",
-                                    "Button Connect - Error connecting to server: ${e.message}"
-                                )
-                                // Update isConnecting state based on failure (optional)
-                                e.printStackTrace()
+                                    // Update isConnecting state based on success (optional)
+                                } catch (e: Exception) {
+                                    Log.e(
+                                        "Connection Error",
+                                        "Button Connect - Error connecting to server: ${e.message}"
+                                    )
+                                    // Update isConnecting state based on failure (optional)
+                                    e.printStackTrace()
+                                }
                             }
                         }
-                    }
-                }
-            else
-                socket.close()
+                    } else
+                        socket.close()
 //                pass
-            },
-                modifier = Modifier.size(width=200.dp, height = 50.dp),
+                },
+                modifier = Modifier.size(width = 200.dp, height = 50.dp),
                 colors = if (isRecording.value) {
                     ButtonDefaults.buttonColors(contentColor = Color.LightGray) // Set custom color for disabled state
                 } else {
